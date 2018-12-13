2845ee5e4ff2a002a29ebd87a5aca5a4b3c9b64b
br0r <viktor@silfverstrom.com>
2018-12-13 12:06:03 +0100

    updated prompt solution

--- solutions/week7/prompt/script.js
+++ solutions/week7/prompt/script.js
@@ -1,23 +1,59 @@
-function asyncPrompt(msg) {
+function popup(element) {
   return new Promise((resolve, reject) => {
-    let body = document.body;
     let popup = document.createElement('div');
     popup.classList.add('popup');
-    let text = document.createElement('p');
-    text.textContent = msg;
-    let input = document.createElement('input');
+
+    let overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+
+    let body = document.body;
+    popup.appendChild(element);
+    body.appendChild(popup);
+    body.appendChild(overlay);
+
     let ok = document.createElement('button');
     ok.textContent = 'OK';
-    ok.addEventListener('click', function() {
+    popup.appendChild(ok);
+
+    overlay.addEventListener('click', function() {
       body.removeChild(popup);
-      resolve(input.value);
+      body.removeChild(overlay);
+      resolve();
     });
 
-    popup.appendChild(text);
-    popup.appendChild(input);
-    popup.appendChild(ok);
-    body.appendChild(popup);
+    ok.addEventListener('click', function() {
+      body.removeChild(popup);
+      body.removeChild(overlay);
+      resolve();
+    });
   });
+}
+
+function asyncAlert(msg) {
+  let body = document.body;
+  let content = document.createElement('div');
+  content.classList.add('content');
+  let text = document.createElement('p');
+  text.textContent = msg;
+  content.appendChild(text);
+
+  return popup(content);
+
+}
+
+function asyncPrompt(msg) {
+  let body = document.body;
+  let content = document.createElement('div');
+  let text = document.createElement('p');
+  text.textContent = msg;
+  let input = document.createElement('input');
+
+  content.appendChild(text);
+  content.appendChild(input);
+  return popup(content)
+    .then(function() {
+      return input.value;
+    });
 }
 
 function syncPrompt(msg) {
@@ -38,17 +74,25 @@ function backgroundLoop() {
 backgroundLoop();
 
 function doPrompt() {
-  let name = syncPrompt('What is your name?');
-  let age = syncPrompt('What is your age?');
-
   asyncPrompt('What is your name?')
     .then(name => {
-      console.log(name);
-      asyncPrompt('What is your age?')
-        .then(age => {
-          console.log(age);
-        });
+      return asyncPrompt('What is your age?')
+      .then(age => {
+        let obj = {
+          name: name,
+          age: age,
+        };  
+        return obj;
+      });
+    })
+    .then(obj => {
+      console.log(obj.name, obj.age);
+      let table = document.createElement('table');
     });
+}
+
+function renderPerson(obj) {
+  console.log(obj.name, obj.age);
 }
 
 document.querySelector('#showprompt').addEventListener('click', doPrompt);
